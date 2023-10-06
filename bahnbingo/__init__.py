65ff17da04812b2f260fbc65b2b1a5122370ebc2
revol-xut <revol-xut@protonmail.com>
2023-10-06 22:39:33 +0200

    fixing image share

--- bahnbingo/__init__.py
+++ bahnbingo/__init__.py
@@ -8,6 +8,7 @@ import uuid
 import copy
 import pyvips
 import io
+import base64
 
 field_config = os.getenv("BAHNBINGO_FIELD_CONFIG")
 bingo_template = os.getenv("BAHNBINGO_BINGO_TEMPLATE")
@@ -23,13 +24,13 @@ with open(bingo_template) as f:
     input_svg = f.read()
 
 
-@app.route('/bingo', methods=['GET'])
+@app.route("/bingo", methods=["GET"])
 def bingo():
     response = jsonify(name_mapping)
     return response
 
 
-@app.route('/share', methods=['POST'])
+@app.route("/share", methods=["POST"])
 def share():
     data = request.get_json(force=True)
 
@@ -43,38 +44,43 @@ def share():
     svg_content_copy = copy.copy(input_svg)
     for i in range(9):
         # validating that it is a valid bingo field
-        if str(data['field'][i]) not in name_mapping.keys():
+        if str(data["field"][i]) not in name_mapping.keys():
             return "Bad User Data", 400
 
         svg_content_copy = svg_content_copy.replace("Test{}".format(str(i)), name_mapping[str(data["field"][i])])
 
-    image = pyvips.Image.svgload_buffer(svg_content_copy.encode(), dpi=300)
+    image = pyvips.Image.svgload_bfuffer(svg_content_copy.encode(), dpi=300)
     image.write_to_file(output_png_path)
 
     response = jsonify({"picture_id": new_uuid})
     return response
 
-@app.route('/share/<image_hash>.png', methods=['GET'])
-def share_image_hash(image_hash):
 
-    fields = image_hash.split(',')
+@app.route("/share/<image_hash>.png", methods=["GET"])
+def share_image_hash(image_hash):
+    current_config_field = base64.b64decode(image_hash).decode()
+    print(current_config_field)
+    fields = current_config_field.split(",")
 
     svg_content_copy = copy.copy(input_svg)
     for i in range(9):
         # validating that it is a valid bingo field
-        if str(fields[i]) not in name_mapping.keys():
+        if not fields[i].isdigit() and str(int(fields[i])) not in name_mapping.keys():
             return "Bad User Data", 400
 
         svg_content_copy = svg_content_copy.replace("Test{}".format(str(i)), name_mapping[str(fields[i])])
-        image = pyvips.Image.svgload_buffer(svg_content_copy.encode(), dpi=300)
+
+    image = pyvips.Image.svgload_buffer(svg_content_copy.encode(), dpi=300)
+    image_buffer = image.pngsave_buffer()
 
     return send_file(
-        io.BytesIO(image.to_bytes), # TODO: fix das mal muss iwie binary oder so
-        mimetype='image/png',
-        as_attachment=True,
-        download_name='%s.png' % pid
+        io.BytesIO(image_buffer),  # TODO: fix das mal muss iwie binary oder so
+        mimetype="image/png",
+        as_attachment=False,
+        download_name="content.png",
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(name_mapping)
     app.run(host=http_host, port=int(http_port))
