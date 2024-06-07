598bda6e7675f643a704d03a27d73485c3978a41
tanneberger <revol-xut@protonmail.com>
2024-06-07 22:14:45 +0200

    new frontend from lucas

--- pkgs/frontend.nix
+++ pkgs/frontend.nix
@@ -1,30 +1,11 @@
-{ pkgs, lib, config, mkYarnPackage, yarn }:
-mkYarnPackage {
+{ pkgs, lib, stdenv }:
+pkgs.stdenv.mkDerivation {
     name = "bahn-bingo-frontend";
     src = ../frontend/.;
-
-    buildInputs = [ yarn ];
-
-    buildPhase = ''
-      #FILE=$(readlink ./deps/bahn-bingo-frontend/node_modules)
-      #rm ./deps/bahn-bingo-frontend/node_modules
-      #mkdir ./deps/bahn-bingo-frontend/node_modules
-      #cp -r $FILE/ ./deps/bahn-bingo-frontend/
-      #cp -r ./node_modules/* ./deps/bahn-bingo-frontend/node_modules/
+    phases = ["unpackPhase" "installPhase" ];
     
-      yarn build
-    '';
-
     installPhase = ''
       mkdir -p $out/bin
-      cp -r ./deps/bahn.bingo/dist/* $out/bin/
+      cp -r ./* $out/bin
     '';
-
-    doDist = false;
-
-
-  meta = with lib; {
-    description = "simple bingo frontend to shit on deutsche bahn";
-    homepage = "https://github.com/revol-xut/bahn.bingo";
-  };
-}
+  }
